a183074654c09922515d85d34bb5840676725d19
Ravi Waghmare <ravikantwaghmare82@gmail.com>
2023-08-18 00:59:19 +0530

    sidebar design and user profile design

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,11 @@
-console.log("welcome")
+console.log("welcome");
+
+function toggleSidebar(){
+	if($(".sidebar").is(":visible")){
+		$(".sidebar").css("display","none")
+		$(".content").css("margin-left","5px")
+	}else{
+		$(".sidebar").css("display","block")
+		$(".content").css("margin-left","18%")
+	}
+}
